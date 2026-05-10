ce1978b209842caee964742f6797468d2faa0a30
Keoni Inniss <ika84465@gmail.com>
2026-05-10 10:25:14 -0400

    Updated app jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import QuestionDetails from "./pages/QuestionDetails";
 import AskQuestion from "./pages/AskQuestion";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/questions/:id" element={<QuestionDetails />} />
         <Route path="/ask" element={<AskQuestion />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
