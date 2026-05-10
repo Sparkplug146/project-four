002a0732759520ce25581d2aa93d542eb150e084
Keoni Inniss <ika84465@gmail.com>
2026-05-10 10:37:12 -0400

    Updated app jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,48 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
 import QuestionDetails from "./pages/QuestionDetails";
 import AskQuestion from "./pages/AskQuestion";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/questions/:id" element={<QuestionDetails />} />
-        <Route path="/ask" element={<AskQuestion />} />
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/ask"
+          element={
+            <ProtectedRoute>
+              <AskQuestion />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/questions/:id"
+          element={
+            <ProtectedRoute>
+              <QuestionDetails />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
