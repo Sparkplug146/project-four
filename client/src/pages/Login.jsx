e320add96c0ea339279a805a8c977c46b5dd3542
Keoni Inniss <ika84465@gmail.com>
2026-05-10 10:23:17 -0400

    Added login jsx file

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,74 @@
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ username, password })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          setError(data.error);
+        } else {
+          login(data.user, data.token);
+          navigate("/");
+        }
+      })
+      .catch(() => setError("Server error"));
+  }
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>DevDen Forum</h1>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+            setError("");
+          }}
+        />
+        <br />
+        <br />
+
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setError("");
+          }}
+        />
+        <br />
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <p>
+        Don’t have an account? <Link to="/register">Register</Link>
+      </p>
+    </div>
+  );
+}
